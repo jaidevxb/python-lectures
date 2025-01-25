f37c4c366c3500f24305fa19128da9b88348d852
Jaidev B <jaidevsboa@gmail.com>
2025-01-25 19:57:10 +0530

    Update lecture2.py

--- lecture2.py
+++ lecture2.py
@@ -79,5 +79,23 @@ elif num > guess:
 else: # can use this also as elif else is not mandatory like c++ (in else if ladder)
     print("Guess is too high")
 
+# Finger Ex lecture 2
+''' Assume you are given a variable named number (has a numerical value). Write a piece of Python code that prints out one of the following strings: 
+positive if the variable number is positive
+negative if the variable number is negative
+zero if the variable number is equal to zero '''
+
+number = int(input("Enter a number : "))
+if number > 0:
+    print(f"{number} is positive")
+elif number == 0:
+    print(f"{number} is equal to zero")
+else:
+    print(f"{number} is negative")
+
+# Enter a number : 10
+# 10 is positive 
+
+
 
 
